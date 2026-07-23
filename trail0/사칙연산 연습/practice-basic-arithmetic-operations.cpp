b0dc20a639ebc60c5d09c9c00912169c9ae99200
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-23 12:37:58 +0900

    [Passed] 사칙연산 연습 (2ms, 0MB)

---
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+
+    cout << (c /= 8) << "\n" ;
+
+    c = a + b;
+
+    cout << (c %= 8);
+
+    return 0;
+}
