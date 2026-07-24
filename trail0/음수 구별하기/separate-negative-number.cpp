c9b9a0f9fc8fa00a3d72f10272a8ba3bdd90817a
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-24 20:41:16 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ trail0/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cout << N;
+    if(N < 0){
+        cout << "\n";
+        cout << "minus";
+    }
+    return 0;
+}
