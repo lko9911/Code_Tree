2b66f4c7f5646f47152bd836bb5cfe147da87429
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-24 21:08:39 +0900

    [Passed] N번 더하기 (2ms, 0MB)

--- trail0/N번 더하기/add-n-times.cpp
+++ trail0/N번 더하기/add-n-times.cpp
@@ -4,11 +4,10 @@ using namespace std;
 int main() {
     int A, N;
     cin >> A >> N;
-    int sum = A;
 
     for(int i=0; i< N; i++){
-        cout << sum+N << endl;
-        sum += N;
+        cout << A+N << endl;
+        A += N;
     } 
     return 0;
 }
