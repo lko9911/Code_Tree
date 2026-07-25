45dfa89892c1552df0a53ffccc57729ffe704f47
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-25 10:16:12 +0900

    [Passed] 일의 자리 배열 (2ms, 0MB)

--- trail0/일의 자리 배열/array-with-units.cpp
+++ trail0/일의 자리 배열/array-with-units.cpp
@@ -5,19 +5,13 @@ int main() {
     int a,b;
     cin >> a >> b;
     
-    int c[10];
-    
-    c[0] = a;
-    c[1] = b;
-    c[2] = (a + b)%10;
+    int c[10] = {a,b};
 
-    for(int i = 3; i<10; i++){
-        c[i] = ((c[i-1] % 10) + (c[i-2] % 10))%10;
-    }
+    for(int i=2; i<10; i++) 
+        c[i] = (c[i-1] + c[i-2])%10;
 
-    for(int i = 0; i<10; i++){
+    for(int i=0; i<10; i++) 
         cout << c[i] << " ";
-    }
-    
+
     return 0;
 }
