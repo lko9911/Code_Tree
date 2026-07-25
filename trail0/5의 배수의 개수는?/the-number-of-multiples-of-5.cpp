b024ed455c7585069680ef337a02ebc2058bc1af
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-26 08:45:41 +0900

    [Passed] 5의 배수의 개수는? (2ms, 0MB)

---
+++ trail0/5의 배수의 개수는?/the-number-of-multiples-of-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4][4];
+    int cnt = 0;
+    for(int i = 0; i<4; i++){
+        for(int j=0; j<4; j++){
+            cin >> arr[i][j];
+            if(arr[i][j]%5==0)
+            cnt++;
+        }
+    }
+
+cout << cnt;
+
+
+    return 0;
+}
