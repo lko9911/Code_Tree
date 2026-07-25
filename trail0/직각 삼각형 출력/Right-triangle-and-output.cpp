e417945bdd75f52ed1cdc506b4b83904f2966107
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-25 13:10:25 +0900

    [Passed] 직각 삼각형 출력 (2ms, 0MB)

--- trail0/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ trail0/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -4,10 +4,9 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int num = 1;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j <= num; j++) cout << "*";
-    num +=2;
+
+    for(int i=0; i<n; i++){
+        for(int j=1; j <= i*2+1; j++) cout << "*";
     cout << endl;
     }
     return 0;
