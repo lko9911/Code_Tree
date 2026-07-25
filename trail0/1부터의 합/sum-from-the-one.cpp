de3feccc45a9e6d1a82a4b181cfa4a55f7f8da7f
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-25 11:30:39 +0900

    [Passed] 1부터의 합 (2ms, 0MB)

--- trail0/1부터의 합/sum-from-the-one.cpp
+++ trail0/1부터의 합/sum-from-the-one.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main() {
     int n;
-    int s=1;
+    int s=0;
     cin >> n;
 
     for(int i = 1; i<=100; i++){
         s += i;
-        if(s > n){
-            cout << i;
+        if(s >= n){
+            cout << i << endl;
             break;
         }
     }
