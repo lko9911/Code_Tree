a77cef58a4e5535b421608c23ee7c29ddc3fd396
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-25 10:02:28 +0900

    [Passed] 제곱하여 출력하기 (1ms, 0MB)

---
+++ trail0/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a[100];
+
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    for(int i=0; i<n; i++){
+        cout << a[i]*a[i] << " ";
+    }
+
+    return 0;
+}
