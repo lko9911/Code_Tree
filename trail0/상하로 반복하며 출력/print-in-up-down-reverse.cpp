e57263390cf94c381d2da9266b6de19fdee526e2
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-26 09:28:29 +0900

    [Passed] 상하로 반복하며 출력 (2ms, 0MB)

--- trail0/상하로 반복하며 출력/print-in-up-down-reverse.cpp
+++ trail0/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -2,27 +2,17 @@
 using namespace std;
 
 int main() {
-    int n, arr[9][9], col[9];
+    int n;
     cin >> n;
 
-    for(int i=1; i<=n; i++){
-        col[i-1] = i;
-    }
-
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(i%2==0)
-            arr[j][i] = col[j];
+            if(j%2==0)
+            cout << i+1;
             else
-            arr[j][i] = col[n-j-1];
+            cout << n-i;
         }
-    }
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout << arr[i][j];
-        }
-        cout << endl;
+        cout <<endl;
     }
 
     return 0;
