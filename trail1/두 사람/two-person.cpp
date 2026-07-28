adbb7faa915654f2f08dd7a6730627ab1a625c36
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-28 11:03:10 +0900

    [Passed] 두 사람 (2ms, 0MB)

---
+++ trail1/두 사람/two-person.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age1, age2; 
+    char sex1, sex2;
+    cin >> age1 >> sex1 >> age2 >> sex2;
+
+    if((sex1 == 'M' && age1 >= 19)  || (sex2 == 'M' && age2 >= 19) ) cout << 1;
+
+    else cout << 0; 
+
+
+    return 0;
+}
