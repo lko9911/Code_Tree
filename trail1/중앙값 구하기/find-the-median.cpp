0aa34622f08ec4f3fc921544f5e34ae4964a43df
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-28 12:44:47 +0900

    [Passed] 중앙값 구하기 (2ms, 0MB)

---
+++ trail1/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >>c;
+
+    if(a >= b)
+        if(a <= c) cout << a;
+        else if(b <= c && c <= a) cout << c;
+        else cout << b;
+    else
+        if(b <= c) cout << b;
+        else if(a <= c && c <= b) cout << c;
+        else cout << a;
+
+    return 0;
+}
