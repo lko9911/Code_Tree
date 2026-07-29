a0504d310853156302a85c39f6f5ae2559091e58
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-29 20:29:36 +0900

    [Passed] 입력받은 수의 합과 평균 (2ms, 0MB)

---
+++ trail1/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a,sum=0;
+    cin >> n;
+
+    for(int i = 0 ; i<n; i++){
+        cin >> a;
+        sum +=a;
+    }
+
+    cout << fixed;
+    cout.precision(1);
+    cout << sum << " " << (double)sum/n;
+    return 0;
+}
