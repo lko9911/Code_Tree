4ceae7861625ed278d5754e5a0ab636e452727e1
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-07-30 10:39:34 +0900

    [Passed] 특정 수 맞히기 (1ms, 0MB)

---
+++ trail1/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while(1){
+        cin >> n;
+        if(n < 25) cout << "Higher"<< endl;
+        else if(n > 25) cout << "Lower" << endl;
+        else {
+            cout << "Good";
+            break;
+    }
+    }
+    return 0;
+}
