61f432d58d827df5ec4816cc938b49360739a26c
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-01 13:24:03 +0900

    [Passed] 9-1 정사각형 (2ms, 0MB)

---
+++ trail1/9-1 정사각형/9-1-square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt=9;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=1; j<=n; j++) {
+            cout << cnt;
+            cnt--;
+            if(cnt == 0) cnt =9;
+        }
+    cout << endl;
+    }
+    return 0;
+}
