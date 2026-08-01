cc7459076eebd2f0e9563b8a94147f7ce8495517
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-01 15:50:15 +0900

    [Passed] 행에 따라 다르게 출력 (2ms, 0MB)

--- trail1/행에 따라 다르게 출력/output-defferently-each-row.cpp
+++ trail1/행에 따라 다르게 출력/output-defferently-each-row.cpp
@@ -2,22 +2,16 @@
 using namespace std;
 
 int main() {
-    int n, cnt=1;
+    int n, cnt=0;
     cin >> n;
 
     for(int i=1; i<=n; i++){
-        if(i%2!=0)
-        for(int j=1; j<=n; j++) {
+        for(int j=1; j<=n; j++){
+            if(i%2!=0) cnt++;
+            else cnt+=2;
             cout << cnt << " ";
-            if(j==n) continue;
-            cnt++;
-        } else for(int j=1; j<=n; j++) {
-            cnt+=2;
-            cout << cnt << " ";
-            if(j==n) cnt++;
         }
-
-    cout << endl;
+        cout <<endl;
     }
 
     return 0;
