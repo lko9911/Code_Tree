ed73216a88695c236382743674d6ac927b3a6d3a
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-01 16:13:17 +0900

    [Passed] 모양대로 수 출력하기 (2ms, 0MB)

--- trail1/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cpp
+++ trail1/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -5,9 +5,11 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i-1; j++) cout << "  ";
-        for(int j=1; j<=n-i+1; j++) cout << n-i+2-j << " ";
+    for(int i=n; i>=1; i--){
+        for(int j=n; j>=1; j--){
+            if(i >= j) cout << j << " ";
+            else cout << "  ";
+        } 
     cout << endl;
     }
 
