65d755de148d41e4a670f5930364bf8636d01f72
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-01 17:24:53 +0900

    [Passed] 짝수의 합 n번 구하기 (2ms, 0MB)

---
+++ trail1/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+     
+    while(n--){
+        int a, b;
+        cin >> a >> b;
+
+        int sum=0;
+        for(int i=a; i<=b; i++)
+            if(i%2==0) sum +=i;
+        
+        cout << sum << endl;
+    }
+    return 0;
+}
