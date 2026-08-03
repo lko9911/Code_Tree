039165c21c6991554d8c16a8ca72c4830a7688a7
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-03 09:47:35 +0900

    [Passed] 배열의 특정 위치 찾기 2 (2ms, 0MB)

---
+++ trail1/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10], sum1=0,sum2=0;
+    
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+
+        if((i+1)%2==0) {
+            sum1 +=arr[i];
+        }
+        else sum2 +=arr[i];
+    }
+
+    cout << (sum1 > sum2 ? sum1-sum2 : sum2-sum1);
+
+    return 0;
+}
