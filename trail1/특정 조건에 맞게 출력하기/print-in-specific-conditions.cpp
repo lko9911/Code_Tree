5a625a734821eb29101186f898ab2a5bcde5354b
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-03 11:41:07 +0900

    [Passed] 특정 조건에 맞게 출력하기 (1ms, 0MB)

---
+++ trail1/특정 조건에 맞게 출력하기/print-in-specific-conditions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], cnt=0;
+
+    for(int i=0; i<100; i++){
+        cin >> arr[i];
+        cnt++;
+        if(arr[i]==0) break;
+    }
+
+    for(int i=0; i<cnt-1; i++){
+        if(arr[i]%2==0)
+        cout << arr[i]/2 << " ";
+        else cout << arr[i] +3 << " ";
+    }
+
+    return 0;
+}
