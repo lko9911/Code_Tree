f4dd769b481840c09fd24ffb43632a5757cc4450
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-04 20:27:00 +0900

    [Passed] 배열의 평균 (2ms, 0MB)

---
+++ trail1/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[2][4];
+    int sum=0;
+
+    for(int i=0; i<2; i++){
+        for(int j=0; j<4; j++){
+            cin >> arr[i][j];
+            sum += arr[i][j];
+        }
+    }
+
+    cout << fixed;
+    cout.precision(1);
+
+    for(int i=0; i<2; i++){
+        int sum=0;
+        for(int j=0; j<4; j++){
+            sum += arr[i][j];
+        }
+        cout << (double)sum/4 << " ";
+    }   
+    cout << endl;
+
+    for(int i=0; i<4; i++){
+            int sum=0;
+            for(int j=0; j<2; j++){
+                sum += arr[j][i];
+            }
+            cout << (double)sum/2 << " ";
+        }
+    cout << endl;
+
+    cout << (double)sum/8;
+
+
+    return 0;
+}
