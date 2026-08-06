e38989a777e490ef086711794fc73f5c93dff536
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-06 09:59:41 +0900

    [Passed] 격자에 점 그리기 (2ms, 0MB)

---
+++ trail1/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m, arr[9][9]={}, cnt=1;
+    cin >> n >> m;
+
+    while(m--){
+        int r,c;
+        cin >> r >> c;
+        arr[r-1][c-1] =cnt++;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+        cout << arr[i][j] << " ";
+    cout << endl;
+    }
+
+    return 0;
+}
