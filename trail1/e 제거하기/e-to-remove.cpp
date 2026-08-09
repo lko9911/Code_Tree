f928f25cdf7c34bc0b88a04e1f7caf81b670df84
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-09 12:42:26 +0900

    [Passed] e 제거하기 (1ms, 0MB)

---
+++ trail1/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.length(); i++){
+        if(s[i]=='e') {
+            s.erase(i,1);
+        break;
+        }
+    }
+
+    cout << s;
+        
+    return 0;
+}
