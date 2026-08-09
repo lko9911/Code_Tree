0e77385d0fd32d954917ada348fc9f93d31d828d
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-09 20:35:46 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void s(int n){
+    for(int i=0; i<n; i++){
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+        s(n);
+   
+
+    return 0;
+}
