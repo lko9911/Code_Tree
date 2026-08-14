df1e48a83dac3a928d48c66abe0d905edf1046c4
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-14 20:28:35 +0900

    [Passed] 순서를 바꾸었을 때 같은 단어인지 판별하기 (2ms, 0MB)

---
+++ trail2/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    if(a==b) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
