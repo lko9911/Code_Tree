0198870d7983f4e3d46f8da8c362a75d4b9a106b
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-15 10:04:03 +0900

    [Passed] 키, 몸무게를 기준으로 정렬 (2ms, 0MB)

---
+++ trail2/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class people{
+    public:
+        string name;
+        int height;
+        int weight;
+        
+        people(){};
+        people(string name, int height, int weight){
+            this-> name = name;
+            this-> height = height;
+            this-> weight = weight;
+        }
+};
+
+bool cmp(people a, people b){
+    if(a.height == b.height) return a.weight > b.weight;
+    return a.height < b.height;
+}
+
+int main() {
+    people person[10];
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> person[i].name >> person[i].height >> person[i].weight; 
+    }
+
+    sort(person, person+n, cmp);
+
+
+    for(int i=0; i<n; i++){
+        cout << person[i].name << " " << person[i].height << " " << person[i].weight << endl; 
+    }
+
+    return 0;
+}
