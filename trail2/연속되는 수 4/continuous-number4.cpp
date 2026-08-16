c72b5077d329eecff620a61fc59f8e3527e50b4b
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-16 09:29:37 +0900

    [Passed] 연속되는 수 4 (2ms, 0MB)

---
+++ trail2/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[1000];
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int cnt=1;
+    int maxcnt=1;
+
+    for(int i=0; i<n-1; i++){
+        if(arr[i] < arr[i+1]) cnt++;
+        else cnt=1;
+
+        if(maxcnt < cnt) maxcnt = cnt;
+    }
+
+    cout << maxcnt;
+
+    return 0;
+}
