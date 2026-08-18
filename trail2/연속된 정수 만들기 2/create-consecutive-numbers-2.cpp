160fb162fc406d1cd11618cc52dd35017c510586
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-18 16:15:44 +0900

    [Passed] 연속된 정수 만들기 2 (2ms, 0MB)

---
+++ trail2/연속된 정수 만들기 2/create-consecutive-numbers-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int pos[3];
+
+int main() {
+    for (int i = 0; i < 3; i++) cin >> pos[i];
+
+    if(pos[0]==pos[1] && pos[1] == pos[2]) cout << 0;
+    else if((pos[0]==(pos[1]+1) && pos[1]==(pos[2]+1)) || ((pos[0]+1)==pos[1] && (pos[1]+1)==pos[2]) || (pos[0]==(pos[1]+1) && pos[2]==(pos[0]+1)) || (pos[2]==(pos[1]+1) && pos[0]==(pos[2]+1)) || (pos[2]==(pos[0]+1) && pos[1]==(pos[2]+1)) || (pos[0]==(pos[2]+1) && pos[1]==(pos[0]+1)))
+    cout << 0;
+
+    else if((pos[0] == (pos[1]+2)) || (pos[1] == (pos[2]+2)) || (pos[1] == (pos[0]+2)) || (pos[2] == (pos[1]+2)))
+    cout << 1;
+    else cout << 2;
+
+
+
+
+    return 0;
+}
