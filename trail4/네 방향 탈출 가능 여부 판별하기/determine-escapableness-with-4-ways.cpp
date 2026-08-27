4be90b0f48aff0caec3f660a3f036c62d6cbd2c7
Lee Gyuwon <160494158+lko9911@users.noreply.github.com>
2026-08-27 10:53:10 +0900

    [Passed] 네 방향 탈출 가능 여부 판별하기 (1ms, 0MB)

--- trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -8,29 +8,21 @@ int dx[4] = {-1,1,0,0}, dy[4] = {0,0,-1,1};
 
 bool visited[100][100];
 
-void bfs(){
-    queue<pair <int,int>> q;
-    q.push({0,0});
-    visited[0][0] = true;
+void dfs(int x, int y){
+    visited[x][y] = true;
+   
+    for(int dir=0; dir<4; dir++){
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
 
-    while(!q.empty()){
+        if(nx < 0 || nx >=n || ny <0 || ny >=m) continue;
+        if(visited[nx][ny]) continue;
+        if(a[nx][ny]==0) continue;
 
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        for(int dir=0; dir<4; dir++){
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if(nx < 0 || nx >=n || ny <0 || ny >=m) continue;
-            if(visited[nx][ny]) continue;
-            if(a[nx][ny]==0) continue;
-
-            visited[nx][ny] = true;
-            q.push({nx,ny});
-        }
+        visited[nx][ny] = true;
+        dfs(nx,ny);
     }
+    
 }
 
 int main() {
@@ -42,7 +34,7 @@ int main() {
         }
     }
 
-    bfs();
+    dfs(0,0);
 
     cout << (visited[n-1][m-1] ? 1 : 0);
 
